fa8712714a024e1a1d79a8d92c22d3f2fbd75fdf
Mark Burnett <mburnett@genome.wustl.edu>
2013-11-05 16:40:02 -0600

    refactor code into ASTBuilder

--- perl/Compiler.pm
+++ perl/Compiler.pm
@@ -9,9 +9,7 @@ use Data::Dumper;
 
 use Compiler::Parser;
 use Compiler::Importer;
-
-use Compiler::AST::Tool;
-use Compiler::AST::Process;
+use Compiler::ASTBuilder;
 
 
 class Compiler {
@@ -28,6 +26,7 @@ class Compiler {
 
 sub execute {
     my $self = shift;
+
     my $parse_tree = Compiler::Parser::parse_tree($self->input_file);
     die 'Syntax error' unless $parse_tree;
     printf("Root parse tree: %s\n", Data::Dumper::Dumper($parse_tree));
@@ -35,51 +34,8 @@ sub execute {
     my $importer = Compiler::Importer->create(
         search_path => $self->search_path);
 
-    my $result = get_process_object($importer, $parse_tree);
-
-    printf("AST: %s\n", Data::Dumper::Dumper($result));
-}
-
-sub get_process_object {
-    my ($importer, $process) = @_;
-
-    die 'not an actual process object'
-        unless $process->{type} eq 'process';
-
-    my @children = get_children($importer,
-        $process->{operations});
-
-    return Compiler::AST::Process->create(
-        operation_type => $process->{operation_type},
-        children => \@children);
-}
-
-sub get_children {
-    my ($importer, $operation_definitions) = @_;
-
-    my @children;
-    for my $op (@$operation_definitions) {
-        my $imported_stuff = $importer->import_file($op->{operation_type});
-        # TODO Make sure we don't infinitely recurse.
-
-        if ($imported_stuff->{type} eq 'tool') {
-            push @children, Compiler::AST::Tool->create(
-                operation_type => $op->{operation_type});
-            # imported stuff will contain inputs, etc.
-
-        } elsif ($imported_stuff->{type} eq 'process') {
-            my @grand_children = get_children($importer,
-                $imported_stuff->{operations});
-
-            push @children, Compiler::AST::Process->create(
-                operation_type => $op->{operation_type},
-                children => \@grand_children);
-
-        } else {
-            confess sprintf("Unknown type: %s", $imported_stuff->{type});
-        }
-    }
-    return @children;
+    my $ast = Compiler::ASTBuilder::build_AST($importer, $parse_tree);
+    printf("AST: %s\n", Data::Dumper::Dumper($ast));
 }
 
 sub search_path {
---
+++ perl/Compiler/ASTBuilder.pm
@@ -0,0 +1,62 @@
+package Compiler::ASTBuilder;
+
+use strict;
+use warnings FATAL => 'all';
+
+use Carp qw(confess);
+use Compiler::AST::Tool;
+use Compiler::AST::Process;
+
+
+sub build_AST {
+    my ($importer, $parse_tree) = @_;
+
+    return _get_process_object($importer, $parse_tree);
+}
+
+
+sub _get_process_object {
+    my ($importer, $process) = @_;
+
+    die 'not an actual process object'
+        unless $process->{type} eq 'process';
+
+    my @children = _get_children($importer,
+        $process->{operations});
+
+    return Compiler::AST::Process->create(
+        operation_type => $process->{operation_type},
+        children => \@children);
+}
+
+sub _get_children {
+    my ($importer, $operation_definitions) = @_;
+
+    my @children;
+    for my $op (@$operation_definitions) {
+        my $imported_stuff = $importer->import_file($op->{operation_type});
+        # TODO Make sure we don't infinitely recurse.
+
+        if ($imported_stuff->{type} eq 'tool') {
+            push @children, Compiler::AST::Tool->create(
+                operation_type => $op->{operation_type});
+            # imported stuff will contain inputs, etc.
+
+        } elsif ($imported_stuff->{type} eq 'process') {
+             my @grand_children = _get_children($importer,
+                 $imported_stuff->{operations});
+
+             push @children, Compiler::AST::Process->create(
+                 operation_type => $op->{operation_type},
+                 children => \@grand_children);
+
+        } else {
+            confess sprintf("Unknown type: %s",
+                $imported_stuff->{type});
+        }
+    }
+    return @children;
+}
+
+
+1;
