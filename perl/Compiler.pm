bef2bd149afc9c70f3bfa4162743a2cb80200f1b
Mark Burnett <mburnett@genome.wustl.edu>
2013-11-05 13:53:06 -0600

    basic recursive import

--- perl/Compiler.pm
+++ perl/Compiler.pm
@@ -30,8 +30,26 @@ sub execute {
 
     my $importer = Compiler::Importer->create(
         search_path => $self->search_path);
+
+    my $result = {};
+    go_recursive($importer, $parse_tree, $result);
+
+    print Data::Dumper::Dumper($result);
 }
 
+sub go_recursive {
+    my ($importer, $tree, $result) = @_;
+    for my $op (@{$tree->{operations}}) {
+        my $imported_stuff = $importer->import_file($op->{operation_type});
+        if ($imported_stuff->{type} eq 'tool') {
+            $result->{$op->{operation_type}} = $imported_stuff;
+        } else {
+            my $inner_result = {};
+            go_recursive($importer, $imported_stuff, $inner_result);
+            $result->{$op->{operation_type}} = $inner_result;
+        }
+    }
+}
 
 sub search_path {
     return ['tool-definitions', 'process-definitions'];
