9aacf3aca60f8dbb52a5305ac9aab17c9c9b7471
Mark Burnett <mburnett@genome.wustl.edu>
2013-11-08 10:37:37 -0600

    flesh out sampe tool

--- perl/Tool/Aligner/Bwa/Sampe.pm
+++ perl/Tool/Aligner/Bwa/Sampe.pm
@@ -5,10 +5,15 @@ use warnings FATAL => 'all';
 
 use UR;
 
+
 class Tool::Aligner::Bwa::Sampe {
     is => 'Command::V2',
+
     has_input => [
         alignment_index => {
+            is => "File",
+        },
+        unaligned_bam => {
             is => "File",
         },
         first_sai => {
@@ -17,13 +22,12 @@ class Tool::Aligner::Bwa::Sampe {
         second_sai => {
             is => "File",
         },
+
         max_insert_size => {
             is => "Number",
         },
-        unaligned_bam => {
-            is => "File",
-        },
     ],
+
     has_output => [
         output_file => {
             is => "File",
@@ -34,17 +38,30 @@ class Tool::Aligner::Bwa::Sampe {
 sub execute {
     my $self = shift;
 
-    printf("Running sampe with params:\nmax_insert_size:%s\n", $self->max_insert_size);
-    $self->set_outputs();
+    $self->output_file($self->_create_output_filename);
+    IPC::Run::run($self->command_line, '>', $self->output_file);
 
     return 1;
 }
 
-sub set_outputs {
+sub command_line {
     my $self = shift;
 
-    $self->output_file('Bar');
+    return ['bwa', 'sampe',
+        $self->alignment_index,
+        $self->first_sai,
+        $self->second_sai,
+        $self->unaligned_bam,
+        $self->unaligned_bam];
+}
+
+
+sub _create_output_filename {
+    my $self = shift;
+
+    my $fh = File::Temp->new(UNLINK => 0, SUFFIX => '.sam');
+
+    return $fh->filename;
 }
 
 1;
-
