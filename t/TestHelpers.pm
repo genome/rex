cbdb02f646a0cffbc3ed56ef66333b2f2d0afddf
David Morton <davidlmorton@gmail.com>
2013-11-09 11:50:57 -0600

    Factor more out to TestHelpers

--- t/TestHelpers.pm
+++ t/TestHelpers.pm
@@ -6,12 +6,42 @@ use warnings FATAL => 'all';
 require Exporter;
 our @ISA = qw(Exporter);
 our @EXPORT_OK = qw(
+    run_basic_test
     diff_ast_files
     diff_xml_files
 );
 
 use Test::More;
+use File::Temp;
+
 use Genome::Utility::Test qw(compare_ok);
+
+use UR;
+use Compiler;
+
+sub run_basic_test {
+    my $test_file = shift;
+
+    (my $input_file = $test_file) =~ s/\.t$/.gms/;
+    my $output_directory = File::Temp::tempdir(CLEANUP => 1);
+    my $cmd = Compiler->create(
+        input_file => $input_file,
+        output_directory => $output_directory,
+    );
+
+    ok($cmd->execute, 'command ran') || die;
+
+    my $test_data_directory = $test_file . '.data';
+    diff_ast_files(
+        File::Spec->join($test_data_directory, 'ast'),
+        File::Spec->join($output_directory, 'ast')
+    );
+    diff_xml_files(
+        File::Spec->join($test_data_directory, 'workflow.xml'),
+        File::Spec->join($output_directory, 'workflow.xml')
+    );
+    return $test_data_directory, $output_directory;
+}
 
 sub diff_ast_files {
     my ($blessed, $new) = @_;
--- t/Tests/Unambiguous.t
+++ t/Tests/Unambiguous.t
@@ -1,34 +1,12 @@
 use strict;
 use warnings FATAL => 'all';
 
-use UR;
-
 use Test::More;
-use File::Temp;
 
 use TestHelpers qw(
-    diff_ast_files
-    diff_xml_files
-);
-use Compiler;
-
-(my $input_file = __FILE__) =~ s/\.t$/.gms/;
-my $test_data_directory = __FILE__ . '.data';
-my $output_directory = File::Temp::tempdir(CLEANUP => 1);
-
-my $cmd = Compiler->create(
-    input_file => $input_file,
-    output_directory => $output_directory,
+    run_basic_test
 );
 
-ok($cmd->execute, 'command ran') || die;
-diff_ast_files(
-    File::Spec->join($test_data_directory, 'ast'),
-    File::Spec->join($output_directory, 'ast')
-);
-diff_xml_files(
-    File::Spec->join($test_data_directory, 'workflow.xml'),
-    File::Spec->join($output_directory, 'workflow.xml')
-);
+run_basic_test(__FILE__);
 
 done_testing();
