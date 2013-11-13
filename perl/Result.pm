3569690c7e06b38b16d747459f79189cc2a29f13
Mark Burnett <mburnett@genome.wustl.edu>
2013-11-13 16:39:35 -0600

    sketch lookup implementation for Result

--- perl/Result.pm
+++ perl/Result.pm
@@ -7,6 +7,12 @@ use Genome;
 use UR;
 use Carp qw(confess);
 
+use Manifest::Reader;
+use File::Spec qw();
+use Memoize qw();
+use Params::Validate qw();
+
+use JSON qw();
 
 class Result {
     table_name => 'experimental.result',
@@ -27,29 +33,102 @@ class Result {
             is => 'Text',
             len => 255,
         },
-        inputs => {
-            is => 'HASH',
-            is_optional => 1,
-            is_transient => 1,
-        },
         allocation => {
             is => 'Genome::Disk::Allocation',
             id_by => 'allocation_id',
         },
-    ],
 
-    has_optional => [
         lookup_hash => {
             is => 'Text',
             len => 32
         },
+    ],
 
+    has_optional => [
         test_name => {
             is => 'Text',
             len => '255',
         },
     ],
 };
+
+
+sub lookup {
+    my $class = shift;
+    my %params = Params::Validate::validate(@_, {
+            inputs => { type => Params::Validate::HASHREF },
+            test_name => { type => Params::Validate::HASHREF, optional => 1 },
+            tool_class_name => { type => Params::Validate::SCALAR },
+        });
+
+    $class->_validate_tool_inputs(tool_class_name => $params{tool_class_name},
+        inputs => $params{inputs});
+
+    my $force_scalar = $class->get(tool_class_name => $params{tool_class_name},
+        lookup_hash => $class->calculate_lookup_hash($params{inputs}),
+        test_name => $params{test_name});
+    return $force_scalar;
+}
+
+sub _validate_tool_inputs {
+    my $class = shift;
+    my %params = Params::Validate::validate(@_, {
+            inputs => { type => Params::Validate::HASHREF },
+            tool_class_name => { type => Params::Validate::SCALAR },
+        });
+
+    require $params{tool_class_name};
+    $params{tool_class_name}->validate_inputs($params{inputs});
+
+    return;
+}
+
+sub calculate_lookup_hash {
+    my ($self, $inputs) = @_;
+
+    $self->_validate_inputs_structure($inputs);
+
+    my $json = JSON->new();
+    $json->canonical([1]);
+    my $json_string = $json->encode($inputs);
+
+    return Genome::Sys->md5sum_data($json_string);
+}
+
+
+sub _validate_inputs_structure {
+    my ($self, $inputs) = @_;
+
+    # XXX Make sure each value is a scalar or arrayref of scalars
+
+    return;
+}
+
+sub create_from_manifest {
+    my $class = shift;
+}
+
+
+sub path_to {
+    my ($self, $tag) = @_;
+
+    return $self->manifest->path_to($tag);
+}
+
+
+sub manifest {
+    my $self = shift;
+
+    return Manifest::Reader->create(manifest_path => $self->_manifest_path);
+}
+Memoize::memoize('manifest');
+
+sub _manifest_path {
+    my $self = shift;
+
+    return File::Spec->join($self->allocation->absolute_path,
+        'manifest.xml');
+}
 
 
 1;
---
+++ t/Result.t
@@ -0,0 +1,11 @@
+use strict;
+use warnings FATAL => 'all';
+
+use Test::More;
+use Test::Exception;
+
+
+use_ok('Result');
+
+
+done_testing;
