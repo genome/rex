27d054e971c1111856f0522d1a6c7a4c068cb99e
Mark Burnett <mburnett@genome.wustl.edu>
2013-11-15 16:02:35 -0600

    extract allocation factory

--- perl/Factory/ManifestAllocation.pm
+++ perl/Factory/ManifestAllocation.pm
@@ -7,22 +7,15 @@ use Manifest::Reader;
 use Manifest::Writer;
 use File::Spec qw();
 use Params::Validate qw();
-use Data::UUID qw();
 
-use Genome::Disk::Allocation;
-
+use Factory::Allocation;
 
 sub from_manifest {
     my $manifest_file = shift;
 
     my $reader = Manifest::Reader->create(manifest_file => $manifest_file);
-    my $owner = Genome::Sys->current_user;
-    my $allocation = Genome::Disk::Allocation->create(
-        allocation_path => _generate_allocation_path(),
-        disk_group_name => 'info_genome_models',
-        owner_class_name => $owner->class,
-        owner_id => $owner->id,
-        kilobytes_requested => $reader->total_kilobytes);
+    my $allocation = Factory::Allocation::from_kilobytes_requested(
+        $reader->total_kilobytes);
 
     my $writer = Manifest::Writer->create(
         manifest_file => File::Spec->join($allocation->absolute_path,
@@ -38,12 +31,5 @@ sub from_manifest {
 
     return $allocation;
 }
-
-sub _generate_allocation_path {
-    my $uuid = Data::UUID->new->create_hex;
-    return File::Spec->join('model_data',
-        substr($uuid, 2, 3), substr($uuid, 5));
-}
-
 
 1;
