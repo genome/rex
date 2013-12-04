52cd500bd6201f0c6a9374a2229ba1538c46d40b
David Morton <davidlmorton@gmail.com>
2013-12-03 19:27:06 -0600

    First attempt at shortcutting
    This attempt is failing in an unknown way in legacy workflow using lsf.

--- perl/Tool/Detail/Base.pm
+++ perl/Tool/Detail/Base.pm
@@ -2,6 +2,10 @@ package Tool::Detail::Base;
 use Moose;
 use warnings FATAL => 'all';
 
+use Result;
+use Result::Input;
+use Result::Output;
+
 use Tool::Detail::AttributeSetter;
 with 'WorkflowCompatibility::Role';
 
@@ -9,6 +13,66 @@ with 'WorkflowCompatibility::Role';
 has_contextual_param 'test_name';
 has_contextual_param '_process';
 has_contextual_param '_step_label';
+
+sub status_message {
+    my $self = shift;
+    my $str = shift;
+
+    printf($str . "\n", @_);
+    return;
+}
+
+sub shortcut {
+    my $self = shift;
+
+    $self->status_message('Attempting to shortcut %s with test name (%s)',
+        ref $self, $self->test_name);
+
+    my $result = Result->lookup(inputs => $self->_inputs_as_hashref,
+        tool_class_name => ref $self, test_name => $self->test_name);
+
+    if ($result) {
+        $self->status_message('Found matching result with lookup hash (%s)',
+            $result->lookup_hash);
+        $self->_set_outputs_from_result($result);
+
+        $self->_create_process_step($result);
+        return 1;
+
+    } else {
+        $self->status_message('No matching result found for shortcut');
+        return;
+    }
+}
+
+sub _set_outputs_from_result {
+    my ($self, $result) = @_;
+
+    for my $output ($result->outputs) {
+        my $name = $output->name;
+        $self->$name($output->value_id);
+    }
+
+    return;
+}
+
+sub _create_process_step {
+    my ($self, $result) = @_;
+
+    $self->_translate_inputs('_process', '_step_label');
+    ProcessStep->create(process => $self->_process, result => $result,
+        label => $self->_step_label);
+
+    return;
+}
+
+
+sub execute {
+    my $self = shift;
+
+    die "not implemented yet";
+}
+
 
 
 sub inputs {
