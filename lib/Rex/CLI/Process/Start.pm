96c162bd364d89cd9d113c21b650ac22bd8982ef
David Morton <davidlmorton@gmail.com>
2014-02-13 07:25:41 -0600

    Add -l option to rex process start

--- lib/Rex/CLI/Process/Start.pm
+++ lib/Rex/CLI/Process/Start.pm
@@ -6,6 +6,8 @@ use warnings FATAL => 'all';
 use Procera::Compiler;
 use Procera::Runner;
 use Procera::SourceFile qw(file_path);
+use Procera::Factory::Storage;
+use Procera::Factory::Persistence;
 
 use File::Spec qw();
 use File::Temp qw(tempdir);
@@ -31,6 +33,23 @@ has 'inputs_files' => (
     cmd_aliases => ['inputs-file'],
     documentation => 'A tab-delimited inputs-file',
 );
+has 'run_local_and_wait' => (
+    traits => ['Getopt'],
+    is => 'ro',
+    isa => 'Bool',
+    cmd_flag => 'l',
+    cmd_aliases => ['run-local-and-wait'],
+    documentation => 'Runs the process locally (using tmp storage) and waits at the end.',
+);
+
+has _outputs => (
+    is => 'rw',
+    isa => 'HashRef',
+);
+
+has _runner => (
+    is => 'rw',
+);
 
 has _workflow_file => (
     is => 'rw',
@@ -45,10 +64,47 @@ has _inputs_file => (
 sub run {
     my $self = shift;
 
+    $self->setup_environment();
+
     $self->compile();
     $self->run_process();
+
+    if ($self->run_local_and_wait) {
+        $self->announce_outputs();
+        print "\nYou've set the flag to wait after processing... press enter to finish (will clean up all temporary files)\n";
+        <STDIN>;
+    }
 }
 
+sub announce_outputs {
+    my $self = shift;
+
+    my $storage = Procera::Factory::Storage::create($self->_runner->_storage_type);
+    my $persistence = Procera::Factory::Persistence::create($self->_runner->_persistence_type);
+    my $translator = Procera::Translator->new(
+        storage => $storage,
+        persistence => $persistence,
+    );
+    for my $output_name (keys %{$self->_outputs}) {
+        no warnings;
+        printf "%s is (%s)\n", $output_name,
+            $translator->resolve_scalar_or_url($self->_outputs->{$output_name});
+    }
+    return;
+}
+
+sub setup_environment {
+    my $self = shift;
+
+    if ($self->run_local_and_wait) {
+        $ENV{UR_DBI_NO_COMMIT} = 1;
+        $ENV{NO_LSF} = 1;
+        delete $ENV{WF_USE_FLOW};
+        delete $ENV{AMBER_URL};
+        delete $ENV{ALLOCATION_URL};
+    }
+    return;
+}
 
 sub compile {
     my $self = shift;
@@ -74,7 +130,8 @@ sub run_process {
         inputs => [$self->_inputs_file, @{$self->inputs_files}],
         process_name => $self->source_path,
     );
-    $runner->execute;
+    $self->_runner($runner);
+    $self->_outputs($runner->execute);
 
     return;
 }
